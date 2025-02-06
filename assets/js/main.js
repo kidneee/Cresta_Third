631abb63802d39439b8e85805071e083fb656e61
kido <skido627@gmail.com>
2025-02-06 15:59:21 +0900

    2/6分

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,15 +93,4 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  //Slick
-  $('.slick').slick({
-    fade: true,
-    autoplay: true,
-    speed: 1500,
-    autoplaySpeed: 2000,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    arrows: false,
-  });
-
 });
